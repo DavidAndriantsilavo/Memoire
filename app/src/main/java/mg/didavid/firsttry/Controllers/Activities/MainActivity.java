5f83d8878088f9554a667c6b7019747d978dd1c1
Nasolo Jacquis Rinah Mbolatina Vahatriniaina <mvnasolojr@gmail.com>
2020-07-13 23:43:53 +0300

    mbl tsy mety tsara

--- app/src/main/java/mg/didavid/firsttry/Controllers/Activities/MainActivity.java
+++ app/src/main/java/mg/didavid/firsttry/Controllers/Activities/MainActivity.java
@@ -1,30 +1,20 @@
 package mg.didavid.firsttry.Controllers.Activities;
 ;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.KeyEventDispatcher;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInApi;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.tabs.TabLayout;
-import com.google.firebase.auth.FirebaseAuth;
 
 import mg.didavid.firsttry.Controllers.Fragments.ActuFragment;
 import mg.didavid.firsttry.Controllers.Fragments.GMapFragment;
@@ -35,6 +25,9 @@ import mg.didavid.firsttry.R;
 
 public class MainActivity extends AppCompatActivity{
 
+    BottomNavigationView navigationView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,14 +35,19 @@ public class MainActivity extends AppCompatActivity{
 
         this.configureToolbar();
 
-        BottomNavigationView navigationView = findViewById(R.id.menu_nav); //associate view with the BottomNavigationView object
+        navigationView = findViewById(R.id.menu_nav); //associate view with the BottomNavigationView object
         navigationView.setOnNavigationItemSelectedListener(selectedListener); //set BottomNavigationView focus onto the selected item
 
         //default view
-        navigationView.setSelectedItemId(R.id.map_navv);
-        itemGMap();
+        accueil();
 
 
+
+    }
+
+    private void accueil() {
+        navigationView.setSelectedItemId(R.id.fil_d_actu_nav);
+        itemActu();
     }
 
     BottomNavigationView.OnNavigationItemSelectedListener selectedListener =
@@ -66,39 +64,55 @@ public class MainActivity extends AppCompatActivity{
 
                          //page actu
                         case R.id.fil_d_actu_nav:
-                            ActuFragment fragment1 = new ActuFragment();
-                            FragmentTransaction frag1 = getSupportFragmentManager().beginTransaction();
-                            frag1.replace(R.id.content_nav, fragment1); //replace default View
-                            frag1.commit();
+                            itemActu();
                             return true;
 
                          //page resto
                         case R.id.resto_nav:
-                            RestoFragment fragment2 = new RestoFragment();
-                            FragmentTransaction frag2 = getSupportFragmentManager().beginTransaction();
-                            frag2.replace(R.id.content_nav, fragment2); //replace default View
-                            frag2.commit();
+                            itemResto();
                             return true;
 
                          //page message
                         case R.id.message_nav:
-                            MessageFragment fragment4 = new MessageFragment();
-                            FragmentTransaction frag4 = getSupportFragmentManager().beginTransaction();
-                            frag4.replace(R.id.content_nav, fragment4); //replace default View
-                            frag4.commit();
+                            itemMessage();
                             return true;
 
                          //page parameters
                         case R.id.parametre_nav:
-                            ParametreFragment fragment5 = new ParametreFragment();
-                            FragmentTransaction frag5 = getSupportFragmentManager().beginTransaction();
-                            frag5.replace(R.id.content_nav, fragment5); //replace default View
-                            frag5.commit();
+                            itemParametres();
                             return true;
                     }
                     return false;
                 }
             };
+
+    private void itemParametres() {
+        ParametreFragment fragment5 = new ParametreFragment();
+        FragmentTransaction frag5 = getSupportFragmentManager().beginTransaction();
+        frag5.replace(R.id.content_nav, fragment5); //replace default View
+        frag5.commit();
+    }
+
+    private void itemMessage() {
+        MessageFragment fragment4 = new MessageFragment();
+        FragmentTransaction frag4 = getSupportFragmentManager().beginTransaction();
+        frag4.replace(R.id.content_nav, fragment4); //replace default View
+        frag4.commit();
+    }
+
+    private void itemResto() {
+        RestoFragment fragment2 = new RestoFragment();
+        FragmentTransaction frag2 = getSupportFragmentManager().beginTransaction();
+        frag2.replace(R.id.content_nav, fragment2); //replace default View
+        frag2.commit();
+    }
+
+    private void itemActu() {
+        ActuFragment fragment1 = new ActuFragment();
+        FragmentTransaction frag1 = getSupportFragmentManager().beginTransaction();
+        frag1.replace(R.id.content_nav, fragment1); //replace default View
+        frag1.commit();
+    }
 
     private void itemGMap() {
         GMapFragment fragment3 = new GMapFragment();
@@ -124,16 +138,15 @@ public class MainActivity extends AppCompatActivity{
         setSupportActionBar(toolbar);
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         //3 - Handle actions on menu items
-        switch (item.getItemId()) {
-            case R.id.menu_activity_main_profile:
-                Intent toProfile =  new Intent(getApplicationContext(), ProfileActivity.class);
-                startActivity(toProfile);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.menu_activity_main_profile) {
+            Intent toProfile = new Intent(getApplicationContext(), ProfileActivity.class);
+            startActivity(toProfile);
+            return true;
         }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/mg/didavid/firsttry/Controllers/Activities/RegisterActivity.java
+++ app/src/main/java/mg/didavid/firsttry/Controllers/Activities/RegisterActivity.java
@@ -4,6 +4,7 @@ package mg.didavid.firsttry.Controllers.Activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 
 import android.content.Intent;
@@ -11,6 +12,8 @@ import android.graphics.Rect;
 import android.os.Bundle;
 
 import android.text.TextUtils;
+import android.util.Log;
+import android.util.SparseIntArray;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -18,6 +21,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -25,6 +29,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.common.collect.RangeMap;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -35,6 +40,7 @@ import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -44,6 +50,7 @@ import mg.didavid.firsttry.R;
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText nom_r, prenom_r, phoneNo_r, motDePasse_r, confirmMotDePasse_r;
+    private TextView nom_ctrl, prenom_ctrl, phoneNO_ctrl, motDePasse_ctrl;
     private Button btnSend;
     private RadioButton male, female;
     private ProgressBar loginProgress;
@@ -71,6 +78,11 @@ public class RegisterActivity extends AppCompatActivity {
         motDePasse_r = findViewById(R.id.editText_password);
         confirmMotDePasse_r = findViewById(R.id.editText_confirm_password);
 
+        nom_ctrl = findViewById(R.id.controle_nom_rg);
+        prenom_ctrl = findViewById(R.id.controle_prenom_rg);
+        phoneNO_ctrl = findViewById(R.id.controle_phoneNo_rg);
+        motDePasse_ctrl = findViewById(R.id.controle_pwd_rg);
+
         documentReference = db.collection("Users").document("Profile");
 
         btnSend = findViewById(R.id.button_send);
@@ -82,25 +94,71 @@ public class RegisterActivity extends AppCompatActivity {
 
 
         btnSend.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("WrongConstant")
             @Override
             public void onClick(View v) {
                 String phone_num = phoneNo_r.getText().toString();
                 String completePhoneNo = M_COUNTRY_CODE + phone_num;
 
-                if(phone_num.isEmpty()){
-                    Toast.makeText(getApplicationContext(), "Entrer numéro !", Toast.LENGTH_LONG).show();
-                }else {
-                    loginProgress.setVisibility(View.VISIBLE);
+                String uName = nom_r.getText().toString();
+                String uLastname = prenom_r.getText().toString();
+                String uMotDePasse = motDePasse_r.getText().toString();
 
-                    //send message to verify phone number
-                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                            completePhoneNo,
-                            1,
-                            TimeUnit.MINUTES,
-                            RegisterActivity.this,
-                            mCallback
-                    );
+                // -- control input --
+
+                //control Name
+                if(TextUtils.isEmpty(uName)){
+                   erreurName();
+                }else {
+                    nom_ctrl.setVisibility(View.INVISIBLE);
                 }
+                //control Lastname
+                if (TextUtils.isEmpty(uLastname)){
+                    erreurLastName();
+                }else {
+                    prenom_ctrl.setVisibility(View.INVISIBLE);
+                }
+                //control phone number
+                if(TextUtils.isEmpty(phone_num)){
+                    erreurEmptyPhoneNo();
+                }else if (phone_num.length() < 9 || phone_num.length() > 10){
+                    erreurLengthPhoneNo();
+                }else if (phone_num.length() == 10){
+                    erreurGivingZero_phoneNo();
+                }else {
+                    phoneNO_ctrl.setVisibility(View.INVISIBLE);
+                }
+                //control password
+                int j = 0;
+                int pwdLength = uMotDePasse.length();
+                if (pwdLength < 6){
+                    erreurPwdLength();
+                }
+
+                //MBOLA TSY METY******************************************************************************
+                if (pwdLength >= 6){
+                    for (j = 0; j < pwdLength; j++) {
+                        char[] ascciiCode = uMotDePasse.toCharArray();
+                        int t = Integer.parseInt(String.valueOf(ascciiCode[j]));
+                        if (((t > 0x40) && (t < 0x5B)) || ((t > 0x60) && (t < 0x7B)) & ((t > 0x29) && (t < 0x3A))) {
+                            continue;
+                        } else {
+                            erreurPwd();
+                            break;
+                        }
+                    }
+                } //*************************************************************************************************
+
+                loginProgress.setVisibility(View.VISIBLE);
+
+                //send message to verify phone number
+                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        completePhoneNo,
+                        1,
+                        TimeUnit.MINUTES,
+                        RegisterActivity.this,
+                        mCallback
+                );
             }
         });
 
@@ -128,6 +186,39 @@ public class RegisterActivity extends AppCompatActivity {
         };
     }
 
+    private void erreurPwdLength() {
+        motDePasse_ctrl.setText("Doit contenir 6 caractères au moins");
+        motDePasse_ctrl.setVisibility(View.VISIBLE);
+    }
+
+    private void erreurPwd() {
+        motDePasse_ctrl.setText("Doit contenir au moins 1 chiffre et une lettre");
+        motDePasse_ctrl.setVisibility(View.VISIBLE);
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private void erreurGivingZero_phoneNo() {
+        phoneNO_ctrl.setText("Vous avez peut-être mis un \"0\" au début");
+        phoneNO_ctrl.setTextColor(R.color.colorPrimary);
+        phoneNO_ctrl.setVisibility(View.VISIBLE);
+    }
+
+    private void erreurLengthPhoneNo() {
+        phoneNO_ctrl.setText("Veillez entrer un numéro de téléphone");
+        phoneNO_ctrl.setVisibility(View.VISIBLE);
+    }
+
+    private void erreurEmptyPhoneNo() {
+        phoneNO_ctrl.setVisibility(View.VISIBLE);
+    }
+
+    private void erreurLastName() {
+        prenom_ctrl.setVisibility(View.VISIBLE);
+    }
+
+    private void erreurName() {
+        nom_ctrl.setVisibility(View.VISIBLE);
+    }
 
 
     private void singinWithAuthCredential(PhoneAuthCredential phoneAuthCredential) {
@@ -155,13 +246,14 @@ public class RegisterActivity extends AppCompatActivity {
         finish();
     }
 
+    @SuppressLint("WrongConstant")
     private void addUser() {
         String uName = nom_r.getText().toString();
         String uLastname = prenom_r.getText().toString();
         String uPhoneNo = "+261" + phoneNo_r.getText().toString();
         String uMotDePasse = motDePasse_r.getText().toString();
 
-        if(!TextUtils.isEmpty(uName)  || !TextUtils.isEmpty(uLastname)  || !TextUtils.isEmpty(uMotDePasse)){
+
             Map<String, Object> profile = new HashMap<>();
             profile.put("name", uName);
             profile.put("last name", uLastname);
@@ -184,10 +276,6 @@ public class RegisterActivity extends AppCompatActivity {
                             loginProgress.setVisibility(View.INVISIBLE);
                         }
                     });
-
-        }else {
-            Toast.makeText(this,"veillez completer tous les champs!", Toast.LENGTH_LONG).show();
-        }
     }
 
     //THE FOLLOWING METHOD IS USED TO DETACH EDIT_TEXT FOCUS WHEN WE CLICK OUTSIDE OF IT
